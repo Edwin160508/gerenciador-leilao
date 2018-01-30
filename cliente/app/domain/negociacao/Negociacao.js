773105c2c61e3a103e24be4f014d88e09b531b87
Lima <edwin.pereira.o.lima@accenture.com>
2018-01-30 18:19:21 -0200

    Ajustando comentarios

--- cliente/app/domain/negociacao/Negociacao.js
+++ cliente/app/domain/negociacao/Negociacao.js
@@ -1,8 +1,24 @@
 class Negociacao {
 
-	constructor(data, quantidade, valor){
-		this.data = data;
-		this.quantidade = quantidade;
-		this.valor = valor;
+	constructor(data, quantidade, valor){//o construtor atribui o set no formato "this._nomeVariavel"
+		this._data = data;
+		this._quantidade = quantidade;
+		this._valor = valor;
+		Object.freeze(this);//Esta instancia será imutável
+	}
+
+	obtemVolume() {
+		return this._quantidade * this._valor;
 	}	
+
+	/*Métodos de encapsulamento*/
+	get data(){ //atribuindo "objeto.getData();" simplifica a chamada do get como "objeto.data;"
+		return this._data;
+	}
+	get quantidade(){
+		return this._quantidade;
+	}
+	get valor(){
+		return this._valor;
+	}
 }
