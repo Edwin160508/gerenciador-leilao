5e8c58baee3a22150cc52edfb633c8281ffa1d34
Lima <edwin.pereira.o.lima@accenture.com>
2018-01-31 15:56:28 -0200

    Incluindo Object.assign() no construtor

--- cliente/app/domain/negociacao/Negociacao.js
+++ cliente/app/domain/negociacao/Negociacao.js
@@ -1,9 +1,7 @@
 class Negociacao {
 
 	constructor(data, quantidade, valor){//o construtor atribui o set no formato "this._nomeVariavel"
-		this._data = new Date(data.getTime());
-		this._quantidade = quantidade;
-		this._valor = valor;
+		Object.assign(this, { _data: new Date(data.getTime()), _quantidade: quantidade, _valor: valor});
 		Object.freeze(this);//Esta instancia será imutável
 	}
 
