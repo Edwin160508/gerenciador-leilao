db30ebc1b372a3916029dbc3a88e20d38c4f33f6
Lima <edwin.pereira.o.lima@accenture.com>
2018-01-31 17:13:03 -0200

    Utilizando conversores parseInt e parseFloat fazendo teste no contole.log

--- cliente/app/controllers/negociacaoController.js
+++ cliente/app/controllers/negociacaoController.js
@@ -3,6 +3,12 @@ class NegociacaoController {
 	adiciona(event){
 		// cancelando a submissão do formulário
 		event.preventDefault();
-		alert('Chamando NegociacaoController!! ');
+		let inputData = document.querySelector('#data');
+		let inputQuantidade = document.querySelector('#quantidade');
+		let inputValor = document.querySelector('#valor');
+
+		console.log(inputData.value);
+		console.log(parseInt(inputQuantidade.value));
+		console.log(parseFloat(inputValor.value));
 	}
 }
