40e9e651a3243a1644dfb6a2a4b27ab8d643941b
Lima <edwin.pereira.o.lima@accenture.com>
2018-02-01 18:49:16 -0200

    Tratamento de Data em browsers

--- cliente/app/controllers/negociacaoController.js
+++ cliente/app/controllers/negociacaoController.js
@@ -13,21 +13,10 @@ class NegociacaoController {
 		// cancelando a submissão do formulário
 		event.preventDefault();
 		//console.log(this._inputData.value);
-		let array = this._inputData.value.split('-');
-		console.log(array);
-		console.log(array.move(1,2));
-		let data = new Date(
-			...this._inputData.value
-			.split('-').move(1,2)
-			.map((item, indice) => item - indice % 2)
-		);
-
 		let data2 = new DateHelper();
-
-		console.log(data);
-		console.log("DateHelper "+data2.modelaDataBrowserAmericano(this._inputData.value));
+		//console.log("DateHelper "+data2.modelaDataBrowserAmericano(this._inputData.value));
 		let negociacao = new Negociacao(
-			this._inputData.value, 
+			data2.modelaDataBrowserAmericano(this._inputData.value), 
 			parseInt(this._inputQuantidade.value), 
 			parseFloat(this._inputValor.value)
 		);
--- cliente/app/helpers/DateHelper.js
+++ cliente/app/helpers/DateHelper.js
@@ -7,10 +7,14 @@ class DateHelper {
 	}
 	
 	modelaDataBrowserAmericano(dataStringFormat){
-		return new Date(...dataStringFormat.split('-').move(1,2).map((item, indice) => item - indice % 2));
+		let idioma = navigator.language || navigator.userLanguage; 		
+		var date;
+		if(idioma === 'en-US'){
+			date = new Date(...dataStringFormat.split('-').move(1,2).map((item, indice) => item - indice % 2));
+		}else{
+			date = new Date(...dataStringFormat.split('-').map((item, indice) => item - indice % 2));
+		}
+		return date;
 	}
 
-	modelaDataBrowser(dataStringFormat){
-		return new Date(...dataStringFormat.split('-').map((item, indice) => item - indice % 2));
-	}
 }
